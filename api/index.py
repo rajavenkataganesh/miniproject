0e813307ee3a6dff953b8ee539ccf8cb3fb7de22
raja venkata ganesh tadiboina <tadiboinaganesh4@gmail.com>
2026-08-31 19:13:01 +0530

    Configure Vercel API serverless entrypoint api/index.py

---
+++ api/index.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+# Add project root directory to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "backend")))
+
+from backend.main import app
